1f9f8d883a3a20840a46f97f9f0697ceda8924f1
João Luis de Bem Fernandes <joaoluisbf@hotmail.com>
2020-06-02 18:44:28 -0300

    class 2 - routes, database access, backend conf.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes'; //get my exported routes
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    
-    console.log("xxxx");
-    
-    response.json([
-        'João',
-        'Teste',
-        'cleiton'
-    ])
+app.use(cors());
+app.use(express.json()); //express will now interpret json bodies. 
 
-});
+app.use(routes);
+app.use("/uploads", express.static(path.resolve(__dirname,"..", 'uploads')));
 
 app.listen(3333);
